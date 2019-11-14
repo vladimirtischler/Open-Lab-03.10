46c282ec760a9e811108ff011f5adce3fd0a691f
vladimirtischler <tischler.vladimir@gmail.com>
2019-11-14 08:45:58 +0100

    sdcs

---
+++ Open-Lab-03.10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Open_Lab_03._10
+{
+    class Program
+    {
+        public static int GetNumberOfCharsInString(char letter, string str)
+        {
+            int i = 0;
+            char[] word = str.ToCharArray();
+            foreach (var a in word)
+            {
+                if (a == letter)
+                {
+                    i++;
+                }
+            }
+            return i;
+        }
+        static void Main(string[] args)
+        {
+            char a = Convert.ToChar(Console.ReadLine());
+            string b = Console.ReadLine();
+            Console.WriteLine(GetNumberOfCharsInString(a, b));
+
+        }
+    }
+}
